eaf6032562104f3531365607b9720e14b16c219a
Sebastian Vallin <82804648+sebastian-vallin@users.noreply.github.com>
2024-08-20 15:33:33 +0200

    Fix accessibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { auth, signIn, signOut } from "@/auth";
 
 export default function Home() {
   return (
-    <main className="prose dark:prose-invert mx-auto px-2 pt-8 md:px-0 md:pt-24">
+    <main className="prose dark:prose-invert prose-a:text-teal-700 dark:prose-a:text-teal-500 mx-auto px-2 pt-8 md:px-0 md:pt-24">
       <h1>
         Welcome to&nbsp;
         <Link href="https://nextjs.org/docs" target="_blank" rel="noreferrer">
@@ -13,29 +13,40 @@ export default function Home() {
       </h1>
       <h2>Features</h2>
       <ul>
-        <li>NEXT.JS App Router</li>
+        <li>
+          NEXT.JS&nbsp;
+          <Link
+            href="https://nextjs.org/docs/app/building-your-application/routing#the-app-router"
+            target="_blank"
+            rel="noreferrer"
+          >
+            App Router
+          </Link>
+        </li>
         <li>Typescript</li>
         <li>
           <Link href="https://tailwindcss.com/docs/installation" target="_blank" rel="noreferrer">
             Tailwindcss
           </Link>
         </li>
-        <ul>
-          <li>
-            <code>
-              <Link href="https://github.com/tailwindlabs/tailwindcss-typography" target="_blank" rel="noreferrer">
-                @tailwindcss/typography
-              </Link>
-            </code>
-          </li>
-          <li>
-            <code>
-              <Link href="https://github.com/jamiebuilds/tailwindcss-animate" target="_blank" rel="noreferrer">
-                tailwindcss-animate
-              </Link>
-            </code>
-          </li>
-        </ul>
+        <li className="block">
+          <ul>
+            <li>
+              <code>
+                <Link href="https://github.com/tailwindlabs/tailwindcss-typography" target="_blank" rel="noreferrer">
+                  @tailwindcss/typography
+                </Link>
+              </code>
+            </li>
+            <li>
+              <code>
+                <Link href="https://github.com/jamiebuilds/tailwindcss-animate" target="_blank" rel="noreferrer">
+                  tailwindcss-animate
+                </Link>
+              </code>
+            </li>
+          </ul>
+        </li>
         <li>
           <Button variant="secondary" asChild className="not-prose">
             <Link href="https://ui.shadcn.com/docs" target="_blank" rel="noreferrer">
@@ -48,9 +59,11 @@ export default function Home() {
             Auth.js
           </Link>
         </li>
-        <ul>
-          <AuthJs />
-        </ul>
+        <li className="block">
+          <ul>
+            <AuthJs />
+          </ul>
+        </li>
       </ul>
       <p className="flex items-center justify-center rounded-xl border border-b border-gray-200 bg-gray-100 bg-gradient-to-b from-zinc-100 p-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
         Get started by editing&nbsp;
